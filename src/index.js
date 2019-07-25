d5cfe7010b4f62d5d5a04210f6a4ec932218d89e
Harry Ho <thienhungho@users.noreply.github.com>
2019-07-25 16:14:23 +0700

    Convert to Webpack

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import 'jquery';
+
+import 'popper.js/dist/popper.js';
+
+import 'bootstrap';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import 'summernote';
+import 'summernote/dist/summernote-bs4.css';
+import 'summernote/dist/summernote-bs4.min.js';
+
+import 'bootstrap-fileinput/css/fileinput.min.css';
+import "bootstrap-fileinput/js/plugins/piexif.min.js";
+import "bootstrap-fileinput/js/plugins/sortable.min.js";
+import "bootstrap-fileinput/js/plugins/purify.min.js";
+import "bootstrap-fileinput/js/fileinput.min.js";
+// import "bootstrap-fileinput/js/locales/LANG.js";
+
+import './css/style.css';
+import './scss/app.scss';
+import './js/main.js';
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#summernote').summernote({
+    $('.summernote').summernote({
         minHeight: 450,
         focus: true
     });
