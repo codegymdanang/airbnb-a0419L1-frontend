a1d51f31d29409d2e618cf83bea6aef9ac768cf8
Harry Ho <thienhungho@users.noreply.github.com>
2019-07-25 16:22:44 +0700

    Remove bootstrap scss

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import 'jquery';
 
 import 'popper.js/dist/popper.js';
 
-import 'bootstrap';
-import 'bootstrap/js/dist/util';
-import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap';
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
@@ -19,6 +19,6 @@ import "bootstrap-fileinput/js/plugins/purify.min.js";
 import "bootstrap-fileinput/js/fileinput.min.js";
 // import "bootstrap-fileinput/js/locales/LANG.js";
 
-import './css/style.css';
 import './scss/app.scss';
+import './css/style.css';
 import './js/main.js';
